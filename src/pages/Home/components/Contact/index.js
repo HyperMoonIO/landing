ffef8a67a6e2f8881707ccdd2e1a79e2ddecea1d
Aksels Ledins <aksels.ledins@gmail.com>
2020-04-25 02:55:29 +0800

    disable contact form

--- src/pages/Home/components/Contact/index.js
+++ src/pages/Home/components/Contact/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import ContactForm from "src/components/ContactForm";
+//import ContactForm from "src/components/ContactForm";
 
 import s from "./style.scss";
 
@@ -8,12 +8,14 @@ const Contact = ({ children }) => (
   <div id="contact" className={s.container}>
     <h3>Contact</h3>
     <p>
-      Soumettez-nous ce que vous souhaitez faire en trois lignes. Nous
+      Soumettez-nous ce que vous souhaitez faire en trois lignes via le chat en bas à droite. Nous
       reviendrons vers vous au plus vite.
     </p>
+    {/*
     <div className={s.contact_form}>
       <ContactForm />
     </div>
+    */}
   </div>
 );
 
